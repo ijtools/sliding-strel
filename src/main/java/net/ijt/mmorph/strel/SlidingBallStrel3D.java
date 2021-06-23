0a3f30eecc3d4f0512f227d94d9ef49adc75ada4
dlegland <david.legland@inrae.fr>
2021-06-23 18:13:52 +0200

    SlidingBallStrel3D: add management of floating-point images

--- src/main/java/net/ijt/mmorph/strel/SlidingBallStrel3D.java
+++ src/main/java/net/ijt/mmorph/strel/SlidingBallStrel3D.java
@@ -5,6 +5,8 @@ package net.ijt.mmorph.strel;
 
 import ij.ImageStack;
 import inra.ijpb.data.image.ByteStackWrapper;
+import inra.ijpb.data.image.Image3D;
+import inra.ijpb.data.image.Images3D;
 import inra.ijpb.morphology.Strel3D;
 import inra.ijpb.morphology.strel.AbstractStrel3D;
 
@@ -148,7 +150,7 @@ public class SlidingBallStrel3D extends AbstractStrel3D
         {
             return slidingDilationUInt8(image);
         }
-        throw new RuntimeException("Requires a stack of ByteProcessor");
+        return slidingDilationFloat(image);
     }
     
     private ImageStack slidingDilationUInt8(ImageStack stack)
@@ -253,6 +255,108 @@ public class SlidingBallStrel3D extends AbstractStrel3D
         return resStack;
     }
 
+    private ImageStack slidingDilationFloat(ImageStack stack)
+    {
+        Image3D array = Images3D.createWrapper(stack);
+        
+        // get array size
+        int sizeX = array.getSize(0);
+        int sizeY = array.getSize(1);
+        int sizeZ = array.getSize(2);
+        
+        // number of non zero elements 
+        int count = elementCount();
+        int nOffsets = this.xOffsets.length;
+        
+        // create local histogram instance
+        final double OUTSIDE = Double.NEGATIVE_INFINITY;
+        LocalHistogramDoubleTreeMap localHisto = new LocalHistogramDoubleTreeMap(count, OUTSIDE);
+
+        // Allocate result
+        ImageStack resStack = stack.duplicate();
+        Image3D res = Images3D.createWrapper(resStack);
+        
+        // temp variables for updating local histogram
+        double vOld, vNew;
+        
+        // Iterate on image rows indexed by z and y
+        for (int z = 0; z < sizeZ; z++)
+        {
+            fireProgressChanged(this, z, sizeZ);
+            
+            for (int y = 0; y < sizeY; y++)
+            {
+                // init local histogram with background values
+                localHisto.reset(count, OUTSIDE);
+
+                // update initialization with visible neighbors
+                for (int x = -intRadius; x < 0; x++)
+                {
+                    // iterate over the list of offsets
+                    for (int i = 0; i < nOffsets; i++)
+                    {
+                        int z2 = z + this.zOffsets[i];
+                        if (z2 < 0 || z2 >= sizeZ)
+                        {
+                            continue;
+                        }
+                        
+                        int y2 = y + this.yOffsets[i];
+                        if (y2 < 0 || y2 >= sizeY)
+                        {
+                            continue;
+                        }
+                        
+                        int x2 = x + this.xOffsets[i];
+                        if (x2 < 0 || x2 >= sizeX)
+                        {
+                            continue;
+                        }
+                        localHisto.replace(OUTSIDE, array.getValue(x2, y2, z2));
+                    }
+                }   
+                
+                // iterate along "middle" values
+                for (int x = 0; x < sizeX; x++)
+                {
+                    // iterate over the list of offsets
+                    for (int i = 0; i < nOffsets; i++)
+                    {
+                        int z2 = z + this.zOffsets[i];
+                        if (z2 < 0 || z2 >= sizeZ)
+                        {
+                            continue;
+                        }
+                        
+                        // current line offset
+                        int y2 = y + this.yOffsets[i];
+                        
+                        // We need to test values only for lines within array bounds
+                        if (y2 >= 0 && y2 < sizeY)
+                        {
+                            // old value
+                            int x2 = x - this.xOffsets[i] - 1;
+                            vOld = (x2 >= 0 && x2 < sizeX) ? array.getValue(x2, y2, z2) : OUTSIDE;
+                            
+                            // new value
+                            x2 = x + this.xOffsets[i];
+                            vNew = (x2 >= 0 && x2 < sizeX) ? array.getValue(x2, y2, z2) : OUTSIDE;
+                            
+                            localHisto.replace(vOld, vNew);
+                        }
+                    }
+
+                    res.setValue(x, y, z, localHisto.getMaxValue());
+                }
+            }
+        }
+
+        // clear the progress bar
+        fireProgressChanged(this, sizeZ, sizeZ);
+        
+        return resStack;
+    }
+
     @Override
     public ImageStack erosion(ImageStack image)
     {
@@ -260,7 +364,7 @@ public class SlidingBallStrel3D extends AbstractStrel3D
         {
             return slidingErosionUInt8(image);
         }
-        throw new RuntimeException("Requires a stack of ByteProcessor");
+        return slidingErosionFloat(image);
     }
     
     private ImageStack slidingErosionUInt8(ImageStack stack)
@@ -355,6 +459,108 @@ public class SlidingBallStrel3D extends AbstractStrel3D
                     }
 
                     res.set(x, y, z, (int) localHisto.getMinValue());
+                }
+            }
+        }
+
+        // clear the progress bar
+        fireProgressChanged(this, sizeZ, sizeZ);
+        
+        return resStack;
+    }
+
+    private ImageStack slidingErosionFloat(ImageStack stack)
+    {
+        Image3D array = Images3D.createWrapper(stack);
+        
+        // get array size
+        int sizeX = array.getSize(0);
+        int sizeY = array.getSize(1);
+        int sizeZ = array.getSize(2);
+        
+        // number of non zero elements 
+        int count = elementCount();
+        int nOffsets = this.xOffsets.length;
+        
+        // create local histogram instance
+        final double OUTSIDE = Double.POSITIVE_INFINITY;
+        LocalHistogramDoubleTreeMap localHisto = new LocalHistogramDoubleTreeMap(count, OUTSIDE);
+
+        // Allocate result
+        ImageStack resStack = stack.duplicate();
+        Image3D res = Images3D.createWrapper(resStack);
+        
+        // temp variables for updating local histogram
+        double vOld, vNew;
+        
+        // Iterate on image rows indexed by z and y
+        for (int z = 0; z < sizeZ; z++)
+        {
+            fireProgressChanged(this, z, sizeZ);
+            
+            for (int y = 0; y < sizeY; y++)
+            {
+                // init local histogram with background values
+                localHisto.reset(count, OUTSIDE);
+
+                // update initialization with visible neighbors
+                for (int x = -intRadius; x < 0; x++)
+                {
+                    // iterate over the list of offsets
+                    for (int i = 0; i < nOffsets; i++)
+                    {
+                        int z2 = z + this.zOffsets[i];
+                        if (z2 < 0 || z2 >= sizeZ)
+                        {
+                            continue;
+                        }
+                        
+                        int y2 = y + this.yOffsets[i];
+                        if (y2 < 0 || y2 >= sizeY)
+                        {
+                            continue;
+                        }
+                        
+                        int x2 = x + this.xOffsets[i];
+                        if (x2 < 0 || x2 >= sizeX)
+                        {
+                            continue;
+                        }
+                        localHisto.replace(OUTSIDE, array.getValue(x2, y2, z2));
+                    }
+                }   
+                
+                // iterate along "middle" values
+                for (int x = 0; x < sizeX; x++)
+                {
+                    // iterate over the list of offsets
+                    for (int i = 0; i < nOffsets; i++)
+                    {
+                        int z2 = z + this.zOffsets[i];
+                        if (z2 < 0 || z2 >= sizeZ)
+                        {
+                            continue;
+                        }
+                        
+                        // current line offset
+                        int y2 = y + this.yOffsets[i];
+                        
+                        // We need to test values only for lines within array bounds
+                        if (y2 >= 0 && y2 < sizeY)
+                        {
+                            // old value
+                            int x2 = x - this.xOffsets[i] - 1;
+                            vOld = (x2 >= 0 && x2 < sizeX) ? array.getValue(x2, y2, z2) : OUTSIDE;
+                            
+                            // new value
+                            x2 = x + this.xOffsets[i];
+                            vNew = (x2 >= 0 && x2 < sizeX) ? array.getValue(x2, y2, z2) : OUTSIDE;
+                            
+                            localHisto.replace(vOld, vNew);
+                        }
+                    }
+
+                    res.setValue(x, y, z, localHisto.getMinValue());
                 }
             }
         }
